22da78482e5fc75a3c615ec369c7768ae3d8da1c
hongjunsun <sunhongjun@gmail.com>
2020-10-30 10:04:59 -0700

    added name getter and setter

--- src/com/learning/blp/model/User.java
+++ src/com/learning/blp/model/User.java
@@ -3,4 +3,12 @@ package com.learning.blp.model;
 public class User {
 	private String name;
 
+	private String getName() {
+		return name;
+	}
+
+	private void setName(String name) {
+		this.name = name;
+	}
+
 }
