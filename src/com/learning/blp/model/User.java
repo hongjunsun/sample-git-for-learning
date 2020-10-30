2b888244d65685e0175df18e40fce8ce9d497143
hongjunsun <sunhongjun@gmail.com>
2020-10-30 11:09:00 -0700

    updated to string method to display age and name

--- src/com/learning/blp/model/User.java
+++ src/com/learning/blp/model/User.java
@@ -14,7 +14,7 @@ public class User {
 
 	@Override
 	public String toString() {
-		return "User [name=" + name + "]";
+		return "User [name=" + name + ", age=" + age + "]";
 	}
 
 	private int getAge() {
